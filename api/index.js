26a2d2d7c91aebfa12d813394bbfc25bfdeac391
GOKUL K <79007791+gokul85@users.noreply.github.com>
2023-02-17 11:45:20 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+mongoose.set("strictQuery", false);
+console.log(process.env.MONGO_URL);
+mongoose
+  .connect("mongodb+srv://Gokul:1zt0vgd9ALqz9VCn@cluster0.io9afw2.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
+if ("production" === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  console.log("Production");
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  );
+}
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
